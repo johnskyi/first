51b215f2d577fde169c64a7f7dc639f8bd6f2101
johnskyi <johnskyi2@yandex.ru>
2021-06-16 20:27:09 +0300

    delete todolist

--- ToDoList/src/main/java/main/repositoryes/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package main.repositoryes;
-
-import main.entityes.TaskEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TaskRepository extends CrudRepository<TaskEntity, Integer> {
-}
